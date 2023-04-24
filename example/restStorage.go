5b1a5f602b478d3a003be1a00b189d2d1f9df0fe
czerospace <czerospace@163.com>
2023-04-24 14:25:19 +0800

    参照 restStorage 接口 实现handerStorage接口

---
+++ example/restStorage.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// 创建任务的 API 处理函数
+func createTaskHandler(c *gin.Context) {
+	var task Task
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(400, gin.H{"error": "Bad Request"})
+		return
+	}
+	taskRegistry := TaskRegistry{}
+	err := taskRegistry.CreateTask(task)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Failed to create task"})
+		return
+	}
+
+	c.JSON(201, gin.H{"message": "Task created successfully"})
+}
+
+// 获取任务列表的 API 处理函数
+func listTaskHandler(c *gin.Context) {
+	taskRegistry := TaskRegistry{}
+	tasks, err := taskRegistry.ListTasks()
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Failed to list tasks"})
+	}
+
+	c.JSON(200, tasks)
+}
+
+func listServicesHandler(c *gin.Context) {
+	c.JSON(200, nil)
+}
+
+func createServicesHandler(c *gin.Context) {
+	c.JSON(200, nil)
+}
+
+func main() {
+	// 初始化 Gin Web 框架
+	r := gin.Default()
+
+	// 注册 API 路由
+	r.POST("/tasks", createTaskHandler)
+	r.GET("/tasks", listTaskHandler)
+	r.GET("/services", listServicesHandler)
+	r.POST("/services", createServicesHandler)
+	r.Any("resource/:type", restHandler)
+
+	// 启动 Web 服务器
+	r.Run(":8080")
+}
+
+type Task struct {
+	Name        string
+	Description string
+}
+
+type TaskRegistry struct {
+}
+
+func (t *TaskRegistry) ListTasks() ([]Task, error) {
+	return []Task{{
+		Name:        "test",
+		Description: "test",
+	}}, nil
+}
+
+func (t *TaskRegistry) CreateTask(task Task) error {
+	return nil
+}
+
+// 模仿 k8s apiserver 源码中的 restStorage
+// 自定义 handerStorage 接口
+type handerStorage interface {
+	List(c *gin.Context)
+	Create(c *gin.Context)
+}
+
+// 定义 TaskStorage 结构体去实现 handerStorage 中的方法
+type TaskStorage struct {
+}
+
+func (t *TaskStorage) List(c *gin.Context) {
+	taskRegistry := TaskRegistry{}
+	tasks, err := taskRegistry.ListTasks()
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Failed to list tasks"})
+	}
+
+	c.JSON(200, tasks)
+}
+
+func (t *TaskStorage) Create(c *gin.Context) {
+	var task Task
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(400, gin.H{"error": "Bad Request"})
+		return
+	}
+	taskRegistry := TaskRegistry{}
+	err := taskRegistry.CreateTask(task)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Failed to create task"})
+		return
+	}
+
+	c.JSON(201, gin.H{"message": "Task created successfully"})
+}
+
+// 定义 TaskStorage 结构体去实现 handerStorage 中的方法
+type ServiceStorage struct {
+}
+
+func (t *ServiceStorage) List(c *gin.Context) {
+	taskRegistry := TaskRegistry{}
+	tasks, err := taskRegistry.ListTasks()
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Failed to list tasks"})
+	}
+
+	c.JSON(200, tasks)
+}
+
+func (t *ServiceStorage) Create(c *gin.Context) {
+	var task Task
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(400, gin.H{"error": "Bad Request"})
+		return
+	}
+	taskRegistry := TaskRegistry{}
+	err := taskRegistry.CreateTask(task)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Failed to create task"})
+		return
+	}
+
+	c.JSON(201, gin.H{"message": "Task created successfully"})
+}
+func restHandler(c *gin.Context) {
+	// eg: http://127.0.0.1:8080/resource/task
+	// 只需要把 task 放入 map 中，由 handerStorage 处理
+	// m := map[string]handerStorage{"task": &TaskStorage{}}
+
+	// 现有又要增加一个 reousrce 类型为  service
+	// 只需 service 实现 handerStorage 接口中的方法，然后加入 map 即可
+	m := map[string]handerStorage{
+		"task":    &TaskStorage{},
+		"service": &ServiceStorage{},
+	}
+	resourceType := c.Param("type")
+	switch c.Request.Method {
+	case "GET":
+		m[resourceType].List(c)
+	case "POST":
+		m[resourceType].Create(c)
+	}
+}
